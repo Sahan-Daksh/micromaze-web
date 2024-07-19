2b55ecd40cc757f06797bfb0957fcd815ce0269a
Sahan Dakshitha <sahandakshitha45@gmail.com>
2024-07-19 18:18:37 +0530

    Added GuidelinesCard

--- src/components/Guidelines.jsx
+++ src/components/Guidelines.jsx
@@ -1,23 +1,18 @@
 import React from 'react'
+import GuidelinesCard from './GuidelinesCard';
 
 const Guidelines = () => {
     return (
         <>
-          <div className="bg-purple-600 h-screen w-full">
+          <div className="bg-purple-600 p-10 w-full">
             <div className="pt-3  ml-[18%]">
               <div className="flex flex-col items-center w-[80%] my-10">
                 <p className="text-6xl">GUIDELINES</p>
               </div>
             </div>
-            <div className='flex pt-3'>
-            <div className="flex-auto pl-[5%]">
-                <div className='flex flex-col items-center bg-slate-50 w-[70px] rounded-lg min-h-[470px] min-w-[600px]'>
-                </div>
-            </div>
-            <div className="flex-auto pl-[5%]">
-                <div className='flex flex-col items-center bg-slate-50 w-[70px] rounded-lg min-h-[470px] min-w-[600px]'>
-                </div>
-            </div>
+            <div className="flex flex-col justify-center items-center lg:flex-row ">
+              <GuidelinesCard/>
+              <GuidelinesCard/>
             </div>
           </div>
         </>

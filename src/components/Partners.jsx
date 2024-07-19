faaa89436866a480288dc50a3250629032facb94
Sahan Dakshitha <sahandakshitha45@gmail.com>
2024-07-19 18:41:59 +0530

    Created PartnerCard - Need to fix size

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PartnerCard from './PartnerCard'
 
 const Partners = () => {
     return (
@@ -9,66 +10,12 @@ const Partners = () => {
                 <p className="text-6xl">Official Partners</p>
               </div>
             </div>
-            <div className='pt-3'>
-                <div className="pl-[18%]">
-                    <div className='flex flex-col items-center bg-slate-50 w-[70px] rounded-lg min-h-[100px] min-w-[1000px]'>
-                    </div>
-                </div>
-                <div className="pl-[18%] pt-4 pb-8">
-                    <div className='flex flex-col items-center bg-purple-400 w-[70px] rounded-lg min-h-[350px] min-w-[1000px]'>
-                    </div>
-                </div>
-            </div>
-            <div className='pt-3'>
-                <div className="pl-[18%]">
-                    <div className='flex flex-col items-center bg-slate-50 w-[70px] rounded-lg min-h-[100px] min-w-[1000px]'>
-                    </div>
-                </div>
-                <div className="pl-[18%] pt-4 pb-8">
-                    <div className='flex flex-col items-center bg-purple-400 w-[70px] rounded-lg min-h-[350px] min-w-[1000px]'>
-                    </div>
-                </div>
-            </div>
-            <div className='pt-3'>
-                <div className="pl-[18%]">
-                    <div className='flex flex-col items-center bg-slate-50 w-[70px] rounded-lg min-h-[100px] min-w-[1000px]'>
-                    </div>
-                </div>
-                <div className="pl-[18%] pt-4 pb-8">
-                    <div className='flex flex-col items-center bg-purple-400 w-[70px] rounded-lg min-h-[350px] min-w-[1000px]'>
-                    </div>
-                </div>
-            </div>
-            <div className='pt-3'>
-                <div className="pl-[18%]">
-                    <div className='flex flex-col items-center bg-slate-50 w-[70px] rounded-lg min-h-[100px] min-w-[1000px]'>
-                    </div>
-                </div>
-                <div className="pl-[18%] pt-4 pb-8">
-                    <div className='flex flex-col items-center bg-purple-400 w-[70px] rounded-lg min-h-[350px] min-w-[1000px]'>
-                    </div>
-                </div>
-            </div>
-            <div className='pt-3'>
-                <div className="pl-[18%]">
-                    <div className='flex flex-col items-center bg-slate-50 w-[70px] rounded-lg min-h-[100px] min-w-[1000px]'>
-                    </div>
-                </div>
-                <div className="pl-[18%] pt-4 pb-8">
-                    <div className='flex flex-col items-center bg-purple-400 w-[70px] rounded-lg min-h-[350px] min-w-[1000px]'>
-                    </div>
-                </div>
-            </div>
-            <div className='pt-3'>
-                <div className="pl-[18%]">
-                    <div className='flex flex-col items-center bg-slate-50 w-[70px] rounded-lg min-h-[100px] min-w-[1000px]'>
-                    </div>
-                </div>
-                <div className="pl-[18%] pt-4 pb-8">
-                    <div className='flex flex-col items-center bg-purple-400 w-[70px] rounded-lg min-h-[350px] min-w-[1000px]'>
-                    </div>
-                </div>
-            </div>
+            <PartnerCard/>
+            <PartnerCard/>
+            <PartnerCard/>
+            <PartnerCard/>
+            <PartnerCard/>
+            <PartnerCard/>
           </div>
         </>
       )

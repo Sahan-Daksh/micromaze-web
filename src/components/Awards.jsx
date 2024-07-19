796720a85e16e465c1a32bbdfed01680d301e1d6
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-07-19 21:49:13 +0530

    Update Hero Section and Navbar Styles
    Adjusted Hero section background and overlay images for better fit.
    Refined Navbar transparency and logo alignment.
    
    Co-Authored-By: Sahan Dakshitha <sahandakshitha45@gmail.com>

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -7,7 +7,7 @@ const Awards = () => {
       <div className="bg-purple-900 w-full p-10 ">
         <div className="pt-3  ml-[18%]">
           <div className="flex flex-col items-center w-[80%] my-10">
-            <p className="text-6xl">AWARDS</p>
+            <p className="text-6xl">Awards</p>
           </div>
         </div>
         <div className="flex flex-col justify-center items-center lg:flex-row ">

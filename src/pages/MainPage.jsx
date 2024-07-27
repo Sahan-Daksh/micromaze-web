23e0c85952c9bdfdbbc7a27b3fd3f68b751dc20c
Sahan Dakshitha <sahandakshitha45@gmail.com>
2024-07-27 15:10:08 +0530

    Removed GSAP Scroll

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -25,7 +25,6 @@ export default function MainPage() {
 
   return (
     <>
-      <SmoothScrollWrapper>
       <Hero />
 
       <Intro />
@@ -52,7 +51,6 @@ export default function MainPage() {
       </AnimatedBackground>
 
       <Footer />
-      </SmoothScrollWrapper>
     </>
   );
 }

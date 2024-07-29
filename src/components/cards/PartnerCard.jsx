30bd616b6e731cf71d3823015b6d484d1a2c750e
Sahan Dakshitha <sahandakshitha45@gmail.com>
2024-07-29 07:29:17 +0530

    Created Partner Image Overlay

--- src/components/cards/PartnerCard.jsx
+++ src/components/cards/PartnerCard.jsx
@@ -3,17 +3,16 @@ import "../../assets/styles/components/cards/PartnerCard.css";
 
 const PartnerCard = ({ partner }) => {
   return (
-    <>
-      <div className="partner-card">
+    <div className="partner-card">
       <img
         src={partner.logo}
         alt={partner.alt}
         className="partner-card-image"
       />
+      <div className="partner-card-overlay">
+        <p className="partner-card-title">{partner.title}</p>
+      </div>
     </div>
-    </>
-    
-    
   );
 };
 

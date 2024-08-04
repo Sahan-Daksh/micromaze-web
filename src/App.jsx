33c9ec1f1994d4e18ae0fcf060443b834588c1c4
Sahan Dakshitha <sahandakshitha45@gmail.com>
2024-08-04 15:55:49 +0530

    Added Loader Animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import MainPage from "./pages/MainPage";
+import React, { useState, useEffect } from 'react';
+import LoadingComponent from './pages/layouts/LoadingComponent';
+import MainPage from './pages/MainPage';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading process
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Adjust the time as needed
+  }, []);
+
   return (
-    <>
-      <MainPage />
-    </>
+    <div>
+      {isLoading ? <LoadingComponent /> : <MainPage />}
+    </div>
   );
 };
 
